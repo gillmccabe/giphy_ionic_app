fe7a70ca5a8ba51a86a28148878de56bfeaeeab1
Gill McCabe <gillianmccabe@outlook.com>
2017-01-03 21:26:20 +0000

    successful get repsonse from giphy search api

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,26 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.controller('DashCtrl', function($scope, $http, $q) {
+
+  var apiKey = "&api_key=dc6zaTOxFJmzC";
+  var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat" + apiKey; 
+
+  function getGifs (){
+    var deferred = $q.defer();
+
+    $http.get(url)
+      .then(
+        function(response){
+          console.log(response);
+        }, 
+        function(error){
+          console.log(error);
+        });
+    return deferred.promise; 
+  }
+  getGifs();
+
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
