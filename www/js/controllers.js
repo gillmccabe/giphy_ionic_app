bfd5fc04e8279cda0359f395316ea2eff237a705
Gill McCabe <gillianmccabe@outlook.com>
2017-01-05 18:54:02 +0000

    added favourites function to discover cards

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -50,15 +50,11 @@ angular.module('starter.controllers', [])
         });
     return deferred.promise; 
   }
+
   discoverGifs();
-
-  // To listen for when this page is active (for example, to refresh data),
-  // listen for the $ionicView.enter event:
-  //
-  //$scope.$on('$ionicView.enter', function(e) {
-  //});
-
-
+  $scope.favouriteGif = function (slug){
+    console.log(slug); 
+  }
 })
 
 
